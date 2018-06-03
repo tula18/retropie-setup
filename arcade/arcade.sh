86d9526c7d174eee8ae895384ac28ff03b5cd2ab
rpc80 <ryanpconnors@gmail.com>
2018-06-03 16:52:34 -0400

    Adds arcade d-pad config to arcade script

--- arcade/arcade.sh
+++ arcade/arcade.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+ARCADE_CONFIG=/opt/retropie/configs/arcade/retroarch.cfg
+
 # Download new launching image and move it to the arcade configs directory
 wget https://raw.githubusercontent.com/ehettervik/es-runcommand-splash/master/arcade/launching.png
 \cp launching.png /opt/retropie/configs/arcade
+
+# Enable d-pad mode
+if [[ -f ${ARCADE_CONFIG} ]]; then
+  for i in $(seq 1 16); do
+    sed -i "s|input_player${i}_analog_dpad_mode = \"0\"|input_player${i}_analog_dpad_mode = \"1\"|" "${ARCADE_CONFIG}";
+  done
+fi
