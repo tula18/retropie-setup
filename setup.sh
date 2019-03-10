53b728b2e7b5bcc817d4ce4981456d3a48e3ed15
rpc80 <ryanpconnors@gmail.com>
2019-03-10 10:32:05 -0400

    add script for disabling overridden auto-save/load state config for nes (lr-fceumm) core

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Run the savefile script
 ./scripts/rpc80_savefile_setup.sh
 
+# Run the auto-state scripts
+./scripts/nes_autosave_fix.sh
+
 # Run the N64 script
 ./n64/n64_setup.sh
 
@@ -12,5 +15,5 @@
 # Run the overclock script
 ./overclock/overclock_setup.sh
 
-# Clear the motd file (needs testing!)
+# Clear the motd file
 truncate -s 0 /etc/motd
